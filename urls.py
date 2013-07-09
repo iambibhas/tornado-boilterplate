a04bada91838a8c02668ae5ec9b5f2257f55e1bc
Bibhas C Debnath <iambibhas@gmail.com>
2013-07-09 22:56:56 +0530

    added shell

--- urls.py
+++ urls.py
@@ -2,10 +2,10 @@ from tornado import web
 from settings import STATIC_ROOT
 from utils import include
 
-from handlers.main.views import HomeHandler
+from apps.main.views import HomeHandler
 
 urls = [
     (r"/", HomeHandler),
     (r"/static/(.*)", web.StaticFileHandler, {"path": STATIC_ROOT}),
 ]
-urls += include(r"/main", "handlers.main.urls")
+urls += include(r"/main", "apps.main.urls")
