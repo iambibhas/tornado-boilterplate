f1ba3120d772367ed005145a2d82e6a04dffd83d
Bibhas C Debnath <iambibhas@gmail.com>
2013-07-09 22:45:00 +0530

    allowing including url with prefix path

--- urls.py
+++ urls.py
@@ -1,21 +1,8 @@
 from tornado import web
 from settings import STATIC_ROOT
+from utils import include
 
 from handlers.main.views import HomeHandler
-
-def include(prefix, module_path):
-    module = __import__(module_path, globals(), locals(), fromlist=["*"])
-    urls = getattr(module, 'urls')
-    final_urls = list()
-    for url in urls:
-        pattern, view = url
-        if not pattern.startswith("/"):
-            pattern = r"%s/%s" % (prefix, pattern)
-        else:
-            pattern = r"%s%s" % (prefix, pattern)
-        final_urls.append((pattern, view))
-    return final_urls
-
 
 urls = [
     (r"/", HomeHandler),
---
+++ utils.py
@@ -0,0 +1,15 @@
+def unpack(first, *rest):
+    return first, rest
+
+def include(prefix, module_path):
+    module = __import__(module_path, globals(), locals(), fromlist=["*"])
+    urls = getattr(module, 'urls')
+    final_urls = list()
+    for url in urls:
+        pattern, rest = unpack(*url)
+        if not pattern.startswith("/"):
+            pattern = r"%s/%s" % (prefix, pattern)
+        else:
+            pattern = r"%s%s" % (prefix, pattern)
+        final_urls.append((pattern,) + rest)
+    return final_urls
