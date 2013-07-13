6ceb4e70113ed2720e011a8cbc57cf62e7d6291e
Bibhas C Debnath <iambibhas@gmail.com>
2013-07-13 23:07:50 +0530

    made settings a dictionary

--- settings.py
+++ settings.py
@@ -4,21 +4,62 @@ import tornado.options
 import os.path
 from tornado.options import define
 
-DEBUG = True
-
 path = lambda root,*a: os.path.join(root, *a)
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 define("port", default=8888, help="run on the given port", type=int)
 tornado.options.parse_command_line()
 
-STATIC_ROOT = path(ROOT, "static")
-TEMPLATES_ROOT = path(ROOT, "templates")
+settings = {
+    # debug: If True the application runs in debug mode
+    'debug': True,
 
-settings = {}
+    # gzip: If True, responses in textual formats will be gzipped automatically.
+    'gzip': True,
 
-settings['debug'] = DEBUG
-settings["static_path"] = STATIC_ROOT
-settings["template_path"] = TEMPLATES_ROOT
-settings["xsrf_cookies"] = True
-settings["cookie_secret"] = "mA8dCYKjsgXpHrlLnxeuDNawitcTEYKRFtMU4GqeWv1hLzN3D7"
+    # log_function: This function will be called at the end of every request
+    # to log the result (with one argument, the RequestHandler object).
+    # The default implementation writes to the logging module's root logger.
+    # May also be customized by overriding Application.log_request.
+    # 'log_function': function_name,
+
+    # ui_modules and ui_methods: http://www.tornadoweb.org/en/stable/overview.html#ui-modules
+
+    # Used by RequestHandler.get_secure_cookie and set_secure_cookie to sign cookies.
+    'cookie_secret': 'uxmRinfK8e7HC59jU4QKAGyEsnecPZHuVGUhmtAqHY5rdScC7FM',
+
+    # login_url: The authenticated decorator will redirect to this url
+    # if the user is not logged in. Can be further customized by
+    # overriding RequestHandler.get_login_url
+    # 'login_url': '/login/',
+
+    # xsrf_cookies: If true, Cross-site request forgery protection will be enabled.
+    'xsrf_cookies': True,
+
+    # twitter_consumer_key, twitter_consumer_secret,
+    # friendfeed_consumer_key, friendfeed_consumer_secret,
+    # google_consumer_key, google_consumer_secret,
+    # facebook_api_key, facebook_secret
+    # Used in the tornado.auth module to authenticate to various APIs.
+
+    # autoescape: Controls automatic escaping for templates.
+    # May be set to None to disable escaping, or to the name of a function
+    # that all output should be passed through.
+    'autoescape': "xhtml_escape",
+
+    # template_path: Directory containing template files.
+    # Can be further customized by overriding RequestHandler.get_template_path
+    'template_path': path(ROOT, "templates"),
+
+    # static_path: Directory from which static files will be served.
+    'static_path': path(ROOT, "static"),
+
+    # static_url_prefix: Url prefix for static files, defaults to "/static/".
+    'static_url_prefix': '/static/',
+
+    # static_handler_class, static_handler_args:
+    # May be set to use a different handler for static files instead of
+    # the default tornado.web.StaticFileHandler.
+    # static_handler_args, if set, should be a dictionary of keyword arguments
+    # to be passed to the handler's initialize method.
+}
